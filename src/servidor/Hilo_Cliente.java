1d76dfe3b25c764924f1b3c97630fdbd3a177fb6
kevinBD1620 <kevinvinicio@gmail.com>
2022-03-10 13:44:37 -0600

    Se agregan los metodos de la clase Hilo_Cliente, que manejan el socket y los hilos del server. FALTAN LOS METODOS DE SetCon y SetDiscon y enviarMensaje

--- src/servidor/Hilo_Cliente.java
+++ src/servidor/Hilo_Cliente.java
@@ -24,14 +24,90 @@ public class Hilo_Cliente extends Thread{
     private String ID;
     
     private boolean Receptor;
+    
+    public Hilo_Cliente (Socket socket, Servidor server){
+        this.server = server;
+        this.socket = socket;
+        try{
+            objectOutputStream = new ObjectOutputStream (socket.getOutputStream());
+            objectInputStream  = new ObjectInputStream  (socket.getInputStream());
             
-           
+        } catch (IOException ex) {
+            System.err.println("Error");
+        }
+    }
+            
+    public void desconectar() {
+        try {
+            socket.close();
+            Receptor=false;
+            
+        }catch (IOException ex) {
+            System.err.println("Error al cortar la comunicacion con el usuario");
+        }
+    }       
     
+    public void escuchar(){
+        Receptor = true;
+        while(Receptor){
+            try {
+                Object aux=objectInputStream.readObject();
+                if (aux instanceof LinkedList){
+                    exe((LinkedList<String>)aux);
+                }
+            } catch (Exception e) {
+                System.err.println("Error");
+            }
+        }
+    }
     
+    public void run() {
+        try{
+            escuchar(); 
+        } catch (Exception ex) {
+            System.err.println("Error en el readline");
+        }
+        desconectar ();
+    }
     
+    public void exe(LinkedList<String> lista){
+        String tipo = lista.get(0);
+        switch (tipo) {
+            case "Solicitud_Con":
+                SetCon(lista.get(1));
+                break;
+            case "SOLICITUD_DISCON":
+                SetDiscon();
+                break;
+            case "MSJ":
+                String destinatario = lista.get(2);
+                server.clientes
+                        .stream()
+                        .filter(h -> (destinatario.equals(h.getID())))
+                        .forEach((h)-> h.enviarMensaje(lista));
+                break;
+            default:
+                break;         
+        }
+    }
     
+    private void enviarMensaje(LinkedList<String> lista){
+        
+    }
 
- 
+    public String getID() {
+        return ID;
+    }
+    
+    private void SetCon(String ID) {
+        
+    }
+    
+   
+     private void SetDiscon() {
+         
+     }
+    
 
    
 
