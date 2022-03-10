5ac354ead1481f9d8667721a7cfd2cacf5bd9a24
kevinBD1620 <kevinvinicio@gmail.com>
2022-03-10 11:29:50 -0600

    Se crean las principales variables de la clase Hilo_Cliente, y sus respectivos imports

--- src/servidor/Hilo_Cliente.java
+++ src/servidor/Hilo_Cliente.java
@@ -1,26 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package servidor;
 
 import java.net.Socket;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.LinkedList;
 
 /**
  *
  * @author kevin
  */
 public class Hilo_Cliente extends Thread{
+    
+    private final Socket socket;
+    
+    private ObjectInputStream objectInputStream;
+    
+    private ObjectOutputStream objectOutputStream;
+    
+    private final Servidor server;
+    
+    private String ID;
+    
+    private boolean Receptor;
+            
+           
+    
+    
+    
+    
 
  
 
-    Hilo_Cliente(Socket socket, Servidor aThis) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public String getID() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+   
 
     
     
