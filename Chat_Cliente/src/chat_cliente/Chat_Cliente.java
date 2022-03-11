c684114941f51b587c46f461af1675dd6a0f3f40
kevinBD1620 <kevinvinicio@gmail.com>
2022-03-10 20:40:10 -0600

    Se agregan los metodos run, desconectar, enviarMensaje, confirmarDesconexion,escuchar,getID, y exe(falta el funcionamiento de este)

--- Chat_Cliente/src/chat_cliente/Chat_Cliente.java
+++ Chat_Cliente/src/chat_cliente/Chat_Cliente.java
@@ -40,12 +40,67 @@ public class Chat_Cliente extends Thread {
         escuchando=true;
         this.start();
     }
+    
+    public void run(){
+        try {
+            socket=new Socket(host, puerto);
+            objectOutputStream=new ObjectOutputStream(socket.getOutputStream());
+            objectInputStream=new ObjectInputStream(socket.getInputStream());
+            System.out.println("Conexion aceptada");
+            this.enviarSolicitudConexion(ID);
+            this.escuchar();
+        } catch (UnknownHostException ex) {
+            JOptionPane.showMessageDialog(ventana, "Se perdio la conexion con el servidor.");
+            System.exit(0);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(ventana, "Se perdio la conexion con el servidor,\n"
+                                                
+                                                 + "Esta aplicación se cerrará.");
+            System.exit(0);
+        }
 
-    void enviarMensaje(String cliente_receptor, String mensaje) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    public void desconectar(){
+        try {
+            objectOutputStream.close();
+            objectInputStream.close();
+            socket.close();  
+            escuchando=false;
+        } catch (Exception e) {
+            System.err.println("Error");
+        }
+    }
+    
+    
+
+    public void enviarMensaje(String cliente_receptor, String mensaje) {
+        LinkedList<String> lista = new LinkedList <> ();
+        lista.add("MSJ");
+        lista.add(ID);
+        lista.add(cliente_receptor);
+        lista.add(mensaje);
+        try {
+            objectOutputStream.writeObject(lista);
+            
+        } catch(IOException ex){
+            System.err.println("Error");
+        }
     }
 
     void confirmarDesconexion() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    String getID() {
+        return ID;
+    }
+
+    private void enviarSolicitudConexion(String ID) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void escuchar() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
